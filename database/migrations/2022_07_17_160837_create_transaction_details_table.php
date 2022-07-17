d257232e2bfe23a7b8a2bccd9f7df5debb3ce161
Yusuf Maruf <achmadusufalmaruf@gmail.com>
2022-07-17 23:13:08 +0700

    db details transaksi maruf

---
+++ database/migrations/2022_07_17_160837_create_transaction_details_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_details', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('transactions_id');
+            $table->string('username');
+            $table->string('id_ktp');
+            $table->string('no_hp');
+            $table->string('email');
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_details');
+    }
+};
